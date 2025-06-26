0ee8d7560b470a451302118f06007a72ce87bd09
Himanshu Naidu <hnaidu36@uw.edu>
2025-06-25 18:58:56 -0700

    Scratch/yago kg 1 (#23)
    * Extend insertion logic to include property counts
    
    * Add constants for Yago kg along with prefix extraction logic
    
    * Add random walk logic

--- src/kg/db/constants.py
+++ src/kg/db/constants.py
@@ -12,6 +12,7 @@ YAGO_ALL_ENTITY_COUNT = 49687885
 PREFIX_PATH = os.path.join(os.path.dirname(__file__), 'info/prefixes.txt')
 ERROR_PATH = os.path.join(os.path.dirname(__file__), 'info/error.txt')
 
+# NOTE: Replace the constants with configuration variables
 # NOTE: These paths are relative to the current file's directory. 
 # They only work if the .ttl files are present in the data directory.
 # These are optional constants, and in real-time setting, these paths would be set in a configuration file.
